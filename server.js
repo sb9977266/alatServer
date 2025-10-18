d6599582aa667211a18bee255e6139ba2a17faef
sb9977266 <sb9977266@gmail.com>
2025-10-18 07:51:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 
 // emails credentials
 const userEmail = "sb9977266@gmail.com";
-const pass = "ahoixmhbtesktlws";
+//const pass = "ahoixmhbtesktlws";
 // 15 APRIL
 
 // Middleware
@@ -113,6 +113,7 @@ app.post("/otp", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
+
 
 
 
